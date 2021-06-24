993052f128f5ab9539d637c8fe43d897329c0e2c
dnadanushka <dnadanushka@gmail.com>
2021-06-24 06:31:32 +0530

    showing todo list

--- lib/UI/screens/home_screen.dart
+++ lib/UI/screens/home_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:starterpp/models/%20todo.dart';
+import 'package:starterpp/providers/todo_list.dart';
 
 import '../../providers/counter_provider.dart';
 
@@ -9,29 +11,43 @@ class MyHomePage extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final _counter = useProvider(counterProvider);
+    final _todoList = useProvider(todoListProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            ListTile(
-              leading: Checkbox(
-                value: false,
-                onChanged: (value) {
-                  _counter.state++;
-                },
-              ),
-              title: Text('Todo Title'),
-            )
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: _todoList.todoList.length,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                leading: Checkbox(
+                  value: _todoList.todoList.elementAt(index).done,
+                  onChanged: (value) {
+                    _todoList.toggleTodo(index);
+                  },
+                ),
+                title: Text(_todoList.todoList.elementAt(index).description),
+              );
+            },
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          _counter.state++;
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: TextField(),
+                  content: Text("Hello World"),
+                );
+              });
         },
         tooltip: 'Increment',
         child: Icon(Icons.add),
